4fce951457a469f1928cb8bbce2c71123b35f527
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-03-04 19:44:34 +0530

    [Tushar Tembhare] Update. Restricted PIN code from starting with alphabets or special characters

--- Pin Code JS/src/pinValidator.js
+++ Pin Code JS/src/pinValidator.js
@@ -1,8 +1,9 @@
-// Validating a 6-digit Indian PIN Code
+// Validating a 6-digit Indian PIN Code (Must start with a digit)
 function validatePinCode(pinCode) {
-    const pinCodeRegex = /^[0-9]{6}$/;
+    const pinCodeRegex = /^[0-9][0-9]{5}$/;
     return pinCodeRegex.test(pinCode);
 }
 
 // Exporting function for testing
 module.exports = validatePinCode;
+
--- Pin Code JS/test/pinValidator.test.js
+++ Pin Code JS/test/pinValidator.test.js
@@ -8,6 +8,14 @@ test('Valid PIN Code: 123456', () => {
     expect(validatePinCode('123456')).toBe(true);
 });
 
+test('Invalid PIN Code: A400088', () => {
+    expect(validatePinCode('A400088')).toBe(false); // Starts with alphabet
+});
+
+test('Invalid PIN Code: #400088', () => {
+    expect(validatePinCode('#400088')).toBe(false); // Starts with special character
+});
+
 test('Invalid PIN Code: 40008', () => {
     expect(validatePinCode('40008')).toBe(false); // Less than 6 digits
 });
